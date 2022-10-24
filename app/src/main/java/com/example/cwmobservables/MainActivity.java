5deaea2f94d0ce9b8a454bd5cf30e8a5a36079a6
Pedzi Tokwe <tokwepedzi@gmail.com>
2022-10-24 18:09:51 +0200

    implemented task model object

--- app/src/main/java/com/example/cwmobservables/MainActivity.java
+++ app/src/main/java/com/example/cwmobservables/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.cwmobservables;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    //UI
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTaskList())
+                .subscribeOn(Schedulers.io())
+
     }
 }
